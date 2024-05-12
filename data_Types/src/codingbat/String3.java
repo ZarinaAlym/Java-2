bdd41cdbea1a4d0d0ca2da861cd1d97cd0b701bc
zarina <zarinaalymbaeva98@gmail.com>
2024-05-12 11:49:18 +0500

    9-may

--- data_Types/src/codingbat/String3.java
+++ data_Types/src/codingbat/String3.java
@@ -1,7 +1,7 @@
 package codingbat;
 
-public class String2 {
+public class String3 {
     public static void main(String[] args) {
-        System.out.println("May");
+        System.out.println("9-may");
     }
 }
